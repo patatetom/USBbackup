74d1c74eaab540f0b975a76d44e2244c4ad1e7c4
lacsaP <patatetom@users.noreply.github.com>
2026-02-20 16:16:56 +0100

    i18n

--- USBbackup.sh
+++ USBbackup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-# le dossier /run/media/$USER n'existe pas avant l'insertion du premier média USB
-# la boucle while [ ! -d "/run/media/$USER" ] loupe la création du sous-dossier
-# le hack consiste à monter/démonter une pseudo disquette vide de 90Kb
-# ce hack est réalisé à l'ouverture de session de l'utilisateur
+# the /run/media/$USER directory does not exist before the first USB insertion
+# the while [ ! -d "/run/media/$USER" ] loop misses creation of the subdirectory
+# the hack is to mount/unmount a 90Kb empty pseudo-floppy
+# this hack is performed at user login
 if [ ! -d "/run/media/$USER" ]
 then
 	floppy=$( mktemp )
@@ -23,51 +23,49 @@ then
 	rm "$floppy"
 fi
 
-# sortie si absence du dossier utilisateur
-# échec de l'étape udisk précédente
+# exit if the user directory is missing
+# previous udisks step failed
 [ ! -d "/run/media/$USER" ] &&
 	echo "" > /dev/stderr
 
-# sous-dossier destiné aux sauvegardes
-# TODO émoticones dans le nom du dossier...
+# subdirectory intended for backups
+# TODO emojis in the folder name...
 repository=USBbackup
 
-# surveillance du dossier utilisateur
-# création d'un sous-dossier à chaque insertion d'un média USB
+# monitor the user directory
 inotifywait -m "/run/media/$USER" |
 	while read -r path event item
 	do
 		if [[ "$event" == "CREATE,ISDIR" ]]
-		# insertion d'un média USB
-		# création d'un nouveau dossier dans /run/media/$USER
+		# USB media inserted
+		# a new directory is created in /run/media/$USER
 		then
-			# normalisation du point de montage
+			# normalize the mount point
 			mount=$( readlink --canonicalize-missing "$path/$item" )
-			# attente du montage
-			# 10s max. par tranche de 2s
+			# wait for the mount
+			# 10s max, in 2s slices
 			for sleep in 1 2 3 4 5
 			do
 				grep -q "$mount" /proc/mounts && continue || sleep 2
 			done
-			# normalisation du chemin de sauvergarde
+			# normalize the backup path
 			backup=$( readlink --canonicalize-missing "$mount/$repository" )
-			# vérification de la présence du sous-dossier USBbackup
+			# check for presence of the USBbackup subdirectory
 			if [ -d "$backup" ]
 			then
-				# identitifant de sauvegarde utilisé par le service dédié
-				# soustraction du padding base64 inutile
+				# backup identifier used by the dedicated service
+				# remove unnecessary base64 padding
 				id=$( base64 <<< "$backup" )
 				systemctl --user start "USBbackup@${id%%=*}.service"
 			fi
 		elif [[ "$event" == "DELETE,ISDIR" ]]
-		# retrait (logique ou physique) d'un média USB
-		# suppression d'un dossier dans /run/media/$USER
+		# logical or physical removal of a USB media
+		# a directory is deleted in /run/media/$USER
 		then
-			# reconstitution de l'identifiant pour arrêt du service dédié
+			# rebuild the identifier to stop the dedicated service
 			mount=$( readlink --canonicalize-missing "$path/$file" )
 			backup=$( readlink --canonicalize-missing "$mount/$repository" )
 			id=$( base64 <<< "$backup" )
 			systemctl --user stop --now "USBbackup@${id%%=*}.service"
 		fi
 	done
-
