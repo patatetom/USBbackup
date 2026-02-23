aaf5ff4cab3a1b099d425608f49cf7cf255eb8e8
lacsaP <patatetom@users.noreply.github.com>
2026-02-23 17:23:11 +0100

    die function

--- installUSBbackup.sh
+++ installUSBbackup.sh
@@ -8,44 +8,39 @@ echo "This script will install USBbackup into your environment if all required c
 read -rsp "Press [Enter] to continue or [Ctrl]-[C] to cancel"$'\n'
 echo
 
+die() { echo "$1"$'\n' >&2; exit ${2:-1}; }
+
 # systemd
 init="$( ps -p 1 -o comm= 2>/dev/null )"
 [[ "$init" != "systemd" ]] &&
-	echo "🔴 systemd is not the system's INIT" >&2 &&
-		exit 1
+	die "🔴 systemd is not the system's INIT"
 echo "✅ systemd"
 
 # udisk2
 ! type -a udisksctl &>/dev/null &&
-	echo "🟠 udisks2 is missing and must be installed" >&2 &&
-		exit 2
+	die "🟠 udisks2 is missing and must be installed" 2
 ! systemctl is-active udisks2 &>/dev/null &&
-	echo "🟡 udisks2 is present but not operational" >&2 &&
-		exit 3
+	die "🟡 udisks2 is present but not operational" 3
 echo "✅ udisks2"
 
 # zstd
 ! type -a zstd &>/dev/null &&
-	echo "🟠 zstd is missing and must be installed" >&2 &&
-		exit 2
+	die "🟠 zstd is missing and must be installed" 2
 echo "✅ zstd"
 
 # base64 (coreutils)
 ! type -a base64 &>/dev/null &&
-	echo "🟠 base64 (coreutils) is missing and must be installed" >&2 &&
-		exit 2
+	die "🟠 base64 (coreutils) is missing and must be installed" 2
 echo "✅ base64"
 
 # inotifywait (inotify-tools)
 ! type -a inotifywait &>/dev/null &&
-	echo "🟠 inotifywait (inotify-tools) is missing and must be installed" >&2 &&
-		exit 2
+	die "🟠 inotifywait (inotify-tools) is missing and must be installed" 2
 echo "✅ inotifywait"
 
 # notify-send (libnotify-bin)
 ! type -a notify-send &>/dev/null &&
-	echo "🟠 notify-send (libnotify-bin) is missing and must be installed" >&2 &&
-		exit 2
+	die "🟠 notify-send (libnotify-bin) is missing and must be installed" 2
 echo "✅ notify-send"
 
 flag=""
@@ -69,8 +64,7 @@ type -a rsync &>/dev/null &&
 	echo "🟡 rsync is missing and must be installed if used" >&2
 
 [[ -z "${flag}" ]] &&
-	echo "🟠 none of the three required tools are present" >&2 &&
-		exit 2
+	die "🟠 none of the three required tools are present" 2
 
 # message
 echo
